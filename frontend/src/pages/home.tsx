ff461a91ae55cc170ea9b89f0d937bf1aac5c838
ejensen2197 <jensen_ethan2112@yahoo.com>
2024-08-14 23:26:46 -0500

    Initial commit

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const Home: React.FC = () => {
+
+    return (
+        <div>
+            <div>
+                <h1>Black-Scholes Model Calculator</h1>
+            </div>
+            <input value='100.00' /> 
+            <button>+</button>
+            <button>-</button>
+        </div>
+    )
+}
+
+export default Home;
